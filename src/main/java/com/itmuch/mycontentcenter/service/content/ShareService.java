f0ea0caecdc2a246150c1d974cd8bed4efb87db8
croot <cgypersonal@163.com>
2021-02-18 22:40:23 +0800

    1.RestTemplate get方法请求

---
+++ src/main/java/com/itmuch/mycontentcenter/service/content/ShareService.java
@@ -0,0 +1,37 @@
+package com.itmuch.mycontentcenter.service.content;
+
+
+import com.itmuch.mycontentcenter.dao.content.ShareMapper;
+import com.itmuch.mycontentcenter.domain.entity.content.Share;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class ShareService {
+    private final ShareMapper shareMapper;
+
+
+    public Share findById(Integer id) {
+        // 获取分享详情
+        Share share = this.shareMapper.selectByPrimaryKey(id);
+        // 发布人id
+        Integer userId = share.getUserId();
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        // 用HTTP GET方法去请求，并返回一个对象
+        String forObject = restTemplate.getForObject("http://localhost:8081/users/1", String.class);
+        System.out.println(forObject);
+    }
+
+
+}
+
