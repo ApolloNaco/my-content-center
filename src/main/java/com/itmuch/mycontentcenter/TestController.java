3334583fd0309de0f4fd3d40ceb7eff459948d56
croot <cgypersonal@163.com>
2021-02-24 23:49:10 +0800

    0.引入Nacos Maven依赖 1.内容中心注册到Nacos上 2.写测试代码测试内容中心DiscoveryClient找到Nacos上的微服务

--- src/main/java/com/itmuch/mycontentcenter/TestController.java
+++ src/main/java/com/itmuch/mycontentcenter/TestController.java
@@ -3,6 +3,8 @@ package com.itmuch.mycontentcenter;
 import com.itmuch.mycontentcenter.dao.content.ShareMapper;
 import com.itmuch.mycontentcenter.domain.entity.content.Share;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +15,9 @@ import java.util.List;
 public class TestController {
     @Autowired
     private ShareMapper shareMapper;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/test")
     public List<Share> testInsert(){
@@ -29,5 +34,17 @@ public class TestController {
         // 2.做查询:查询当前数据库所有的share
         List<Share> shares = shareMapper.selectAll();
         return shares;
+    }
+
+    /**
+     * 测试：服务发现，证明内容中心总能找到用户中心
+     *
+     * @return 用户中心所有实例的地址信息
+     */
+    @GetMapping("test2")
+    public List<ServiceInstance> getInstances() {
+        // 查询指定服务的所有实例的信息
+        // consul/eureka/zookeeper...
+        return this.discoveryClient.getInstances("my-user-center");
     }
 }
