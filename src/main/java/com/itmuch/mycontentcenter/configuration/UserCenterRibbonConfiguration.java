eb1e7d970572a02616d6084bcb3f47af2f52c5a2
croot <cgypersonal@163.com>
2021-02-28 15:03:30 +0800

    1.注释掉Ribbon的单一配置，开启全局配置

--- src/main/java/com/itmuch/mycontentcenter/configuration/UserCenterRibbonConfiguration.java
+++ src/main/java/com/itmuch/mycontentcenter/configuration/UserCenterRibbonConfiguration.java
@@ -1,10 +1,10 @@
 package com.itmuch.mycontentcenter.configuration;
 
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClients;
 import org.springframework.context.annotation.Configuration;
 import ribbonconfiguration.RibbonConfiguration;
 
-//@Configuration
-//@RibbonClient(name = "my-user-center",configuration = RibbonConfiguration.class)
+@Configuration
+@RibbonClients(defaultConfiguration = RibbonConfiguration.class)
 public class UserCenterRibbonConfiguration {
 }
