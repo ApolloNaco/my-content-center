30dd998d906a64484b43159099fba6c7daa0797f
croot <cgypersonal@163.com>
2021-03-01 23:17:53 +0800

    1.通过配置方式的修改feign的日志级别

--- src/main/java/com/itmuch/mycontentcenter/feignclient/UserCenterFeignClient.java
+++ src/main/java/com/itmuch/mycontentcenter/feignclient/UserCenterFeignClient.java
@@ -6,7 +6,8 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "my-user-center", configuration = UserCenterFeignConfiguration.class)
+//@FeignClient(name = "my-user-center", configuration = UserCenterFeignConfiguration.class)
+@FeignClient(name = "my-user-center")
 public interface UserCenterFeignClient {
 
     /**
