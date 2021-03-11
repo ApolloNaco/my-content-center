0b7de2594ceef96b2a79f75fde5fa707d7eee7be
croot <cgypersonal@163.com>
2021-03-11 23:05:02 +0800

    1.sentinel整合feign并撰写流控降级类

--- src/main/java/com/itmuch/mycontentcenter/feignclient/UserCenterFeignClient.java
+++ src/main/java/com/itmuch/mycontentcenter/feignclient/UserCenterFeignClient.java
@@ -1,12 +1,13 @@
 package com.itmuch.mycontentcenter.feignclient;
 
 import com.itmuch.mycontentcenter.domain.dto.user.UserDTO;
+import com.itmuch.mycontentcenter.feignclient.fallback.UserCenterFeignClientFallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "my-user-center", configuration = UserCenterFeignConfiguration.class)
-@FeignClient(name = "my-user-center")
+@FeignClient(name = "my-user-center", fallback = UserCenterFeignClientFallback.class)
 public interface UserCenterFeignClient {
 
     /**
---
+++ src/main/java/com/itmuch/mycontentcenter/feignclient/fallback/UserCenterFeignClientFallback.java
@@ -0,0 +1,15 @@
+package com.itmuch.mycontentcenter.feignclient.fallback;
+
+import com.itmuch.mycontentcenter.domain.dto.user.UserDTO;
+import com.itmuch.mycontentcenter.feignclient.UserCenterFeignClient;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserCenterFeignClientFallback implements UserCenterFeignClient {
+    @Override
+    public UserDTO findById(Integer id) {
+        UserDTO userDTO = new UserDTO();
+        userDTO.setWxNickname("一个默认用户");
+        return userDTO;
+    }
+}
