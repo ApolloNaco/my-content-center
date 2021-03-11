d5d2c5231f124fcfb6edf4332a6cb0e37dd57dc7
croot <cgypersonal@163.com>
2021-03-11 23:10:28 +0800

    1.sentinel整合feign并撰写流控降级类Factory可以打印日志

--- src/main/java/com/itmuch/mycontentcenter/feignclient/UserCenterFeignClient.java
+++ src/main/java/com/itmuch/mycontentcenter/feignclient/UserCenterFeignClient.java
@@ -2,12 +2,16 @@ package com.itmuch.mycontentcenter.feignclient;
 
 import com.itmuch.mycontentcenter.domain.dto.user.UserDTO;
 import com.itmuch.mycontentcenter.feignclient.fallback.UserCenterFeignClientFallback;
+import com.itmuch.mycontentcenter.feignclient.fallbackfactory.UserCenterFeignClientFallbackFactory;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 //@FeignClient(name = "my-user-center", configuration = UserCenterFeignConfiguration.class)
-@FeignClient(name = "my-user-center", fallback = UserCenterFeignClientFallback.class)
+@FeignClient(name = "my-user-center",
+// fallback fallbackFactory只能有一个
+//        fallback = UserCenterFeignClientFallback.class,
+        fallbackFactory = UserCenterFeignClientFallbackFactory.class)
 public interface UserCenterFeignClient {
 
     /**
---
+++ src/main/java/com/itmuch/mycontentcenter/feignclient/fallbackfactory/UserCenterFeignClientFallbackFactory.java
@@ -0,0 +1,24 @@
+package com.itmuch.mycontentcenter.feignclient.fallbackfactory;
+
+import com.itmuch.mycontentcenter.domain.dto.user.UserDTO;
+import com.itmuch.mycontentcenter.feignclient.UserCenterFeignClient;
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class UserCenterFeignClientFallbackFactory implements FallbackFactory<UserCenterFeignClient> {
+    @Override
+    public UserCenterFeignClient create(Throwable cause) {
+        return new UserCenterFeignClient() {
+            @Override
+            public UserDTO findById(Integer id) {
+                log.warn("远程调用被限流/降级了", cause);
+                UserDTO userDTO = new UserDTO();
+                userDTO.setWxNickname("流控/降级返回的用户");
+                return userDTO;
+            }
+        };
+    }
+}
